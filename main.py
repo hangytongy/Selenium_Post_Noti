d58d3f985ab27b68756a1276a688efd23791e1d6
hangytongy <145353377+hangytongy@users.noreply.github.com>
2024-08-28 15:37:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -9,16 +11,20 @@ from datetime import datetime
 import requests
 import json
 
-def chrome_():
+def chrome_(url):
     #set up chrome options
     chrome_options = Options()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--remote-debugging-port=9222")
 
     # Set up the WebDriver
-    driver = webdriver.Chrome(options=chrome_options)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     # Open the webpage
-    driver.get('https://debank.com/quest')
+    driver.get(url)
 
     # Wait for the page to load (you might need to adjust the sleep time or use WebDriverWait for more complex scenarios)
     time.sleep(10)
@@ -39,55 +45,61 @@ def chrome_():
 def get_projects(telegram_bot_api,telegram_chat_id):
     
     projects = []
-
+    urls = ["https://debank.com/quest?status=hot","https://debank.com/quest?status=all"]
+    
     while True:
 
-        elements,timings = chrome_()
-                
-        for i,element in enumerate(elements):
-            if element not in projects:
-                projects.append(element)
-                print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} : {element} added in project list, posted {timings[i]} ago')
-                
-                #send notification
-                message = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} : {element} added in project list, posted {timings[i]} ago'
-                payload = {'chat_id' : telegram_chat_id,'text' : message}
-                
-            elif 'min' in timings[i]:
-                time_ = timings[i]
-                time_ = int(time_[0:time_.index('min')].split()[1])
-                if time_ < 5:
+        for url in urls:
+
+            elements,timings = chrome_(url)
+                    
+            for i,element in enumerate(elements):
+                if element not in projects:
+                    projects.append(element)
+                    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} : {element} added in project list, posted {timings[i]} ago')
+                    
+                    #send notification
+                    message = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} : {element} added in project list, posted {timings[i]} ago'
+                    payload = {'chat_id' : telegram_chat_id,'text' : message}
+                    
+                elif 'min' in timings[i]:
+                    time_ = timings[i]
+                    time_ = int(time_[0:time_.index('min')].split()[1])
+                    if time_ < 5:
+                        #send notification
+                        message = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} : {elements[i]} added in project list, posted {timings[i]} ago'
+                        payload = {'chat_id' : telegram_chat_id,'text' : message}
+    
+                elif 's' in timings[i]:
                     #send notification
                     message = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} : {elements[i]} added in project list, posted {timings[i]} ago'
                     payload = {'chat_id' : telegram_chat_id,'text' : message}
-
-            elif 's' in timings[i]:
-                #send notification
-                message = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} : {elements[i]} added in project list, posted {timings[i]} ago'
-                payload = {'chat_id' : telegram_chat_id,'text' : message}
-
-            else:
-                message = None
-
-        if message != None:
-
-            try: 
-                tele_response = requests.post(telegram_bot_api, json=payload)
-
-                if tele_response.status_code == 200:
-                    print("posted successfully")
+    
                 else:
-                    print(f"failed to post : {tele_response.status_code}, Response : {tele_response.text}")
-
-            except Exception as e:
-                print(f"error posting message {e}")
-                
-        time.sleep(300)
+                    message = None
+    
+            if message != None:
+    
+                try: 
+                    tele_response = requests.post(telegram_bot_api, json=payload)
+    
+                    if tele_response.status_code == 200:
+                        print("posted successfully")
+                    else:
+                        print(f"failed to post : {tele_response.status_code}, Response : {tele_response.text}")
+    
+                except Exception as e:
+                    print(f"error posting message {e}")
+                    
+            time.sleep(300)
 
 if __name__ == '__main__':   
     
     telegram_bot_token = ""
     telegram_bot_api = f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage"
     telegram_chat_id = "" #input slack channel ID here
+    
+    payload = {'chat_id' : telegram_chat_id, 'text' : 'init bot, ignore first notifications'}
+    init = requests.post(telegram_bot_api, json=payload)
     
     get_projects(telegram_bot_api,telegram_chat_id)
